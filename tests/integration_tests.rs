MattX/peroxide

commit cc897c89acd18c6811179c8cf6c717a8aeaa0260

Finish switch to code blocks.

src/compile.rs
@@ -12,19 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use ast::SyntaxElement;
+use ast::{SyntaxElement, Lambda};
 use vm::{Code, Instruction};
 use heap::{RootPtr, Inventory, PtrVec, PoolPtr};
 use environment::RcEnv;
+use value::Value;
+use arena::{Arena, ValRef};
+use std::cell::RefCell;
+
+// TODO in this file -- compilation can't fail so it makes no sense to return Result<> objects.
 
 /// A unit of code corresponding to a function.
 #[derive(Debug, PartialEq, Clone)]
 pub struct CodeBlock {
     pub name: Option<String>,
     pub arity: usize,
-    pub has_rest: bool,
+    pub dotted: bool,
     pub instructions: Vec<Instruction>,
     pub constants: Vec<PoolPtr>,
+    pub code_blocks: Vec<PoolPtr>,
     pub environment: RcEnv,
 }
 
@@ -33,18 +39,22 @@ impl Inventory for CodeBlock {
         for &c in self.constants.iter() {
             v.push(c);
         }
+        for &c in self.code_blocks.iter() {
+            v.push(c);
+        }
     }
 }
 
 impl CodeBlock {
-    pub fn new(name: Option<String>, arity: usize, has_rest: bool, environment: &RcEnv) -> Self {
+    pub fn new(name: Option<String>, arity: usize, dotted: bool, environment: RcEnv) -> Self {
         CodeBlock {
             name,
             arity,
-            has_rest,
+            dotted,
             instructions: vec![],
             constants: vec![],
-            environment: environment.clone(),
+            code_blocks: vec![],
+            environment,
         }
     }
 
@@ -65,78 +75,73 @@ impl CodeBlock {
         self.constants.len() - 1
     }
 }
+
+pub fn compile_toplevel(
+    arena: &Arena,
+    tree: &SyntaxElement,
+    environment: RcEnv
+) -> Result<PoolPtr, String> {
+    let mut code_block = CodeBlock::new(Some("[toplevel]".into()), 0, false, environment);
+
+    // rooted_vec is a bit of a hack to avoid accidentally GCing code blocks.
+    // Why is this needed? CodeBlock objects are immutable once inserted, so we'll have to insert
+    // the toplevel one at the very end of the compilation procedure. This means that any sub-
+    // CodeBlocks aren't rooted even if they are added to the toplevel CodeBlock's code_blocks
+    // array. To alleviate this, we create this additional mutable vector to which we can add
+    // items in progress.
+    let rooted_vec = arena.insert_rooted(Value::Vector(RefCell::new(vec![])));
+
+    compile(arena, tree, &mut code_block, false, rooted_vec.pp())?;
+    code_block.push(Instruction::Finish);
+    Ok(arena.insert(Value::CodeBlock(Box::new(code_block))).0)
+}
+
 pub fn compile(
+    arena: &Arena,
     tree: &SyntaxElement,
     code: &mut CodeBlock,
     tail: bool,
-    toplevel: bool,
-) -> Result<usize, String> {
-    if tail && toplevel {
-        panic!("toplevel expression is not in tail position")
-    }
-    let initial_len = code.code_size();
+    rv: PoolPtr,
+) -> Result<(), String> {
     match tree {
         SyntaxElement::Quote(q) => {
             let idx = code.push_constant(q.quoted.pp());
             code.push(Instruction::Constant(idx));
         }
         SyntaxElement::If(i) => {
-            compile(&i.cond, code, false, false)?;
+            compile(arena, &i.cond, code, false, rv)?;
             let cond_jump = code.code_size();
             code.push(Instruction::NoOp); // Is rewritten as a conditional jump below
-            compile(&i.t, code, tail, false)?;
+            compile(arena, &i.t, code, tail, rv)?;
             let mut true_end = code.code_size();
             if let Some(ref f) = i.f {
                 code.push(Instruction::NoOp);
                 true_end += 1;
-                compile(f, code, tail, false)?;
+                compile(arena, f, code, tail, rv)?;
                 let jump_offset = code.code_size() - true_end;
                 code.replace(true_end - 1, Instruction::Jump(jump_offset));
             }
             code.replace(cond_jump, Instruction::JumpFalse(true_end - cond_jump - 1));
         }
         SyntaxElement::Begin(b) => {
-            compile_sequence(&b.expressions, code, tail)?;
+            compile_sequence(arena, &b.expressions, code, tail, rv)?;
         }
         SyntaxElement::Set(s) => {
-            compile(&s.value, code, false, false)?;
+            compile(arena, &s.value, code, false, rv)?;
             code.push(make_set_instruction(s.altitude, s.depth, s.index));
         }
         SyntaxElement::Reference(r) => {
             code.push(make_get_instruction(r.altitude, r.depth, r.index));
         }
         SyntaxElement::Lambda(l) => {
-            code.push(Instruction::CreateClosure(1));
-            let skip_pos = code.code_size();
-            code.push(Instruction::NoOp); // Will be replaced with over function code
-            code.push(Instruction::CheckArity {
-                arity: l.arity,
-                dotted: l.dotted,
-            });
-            if l.dotted {
-                code.push(Instruction::PackFrame(l.arity));
-            }
-            code.push(Instruction::ExtendFrame(l.defines.len()));
-            code.push(Instruction::ExtendEnv);
-            //code.push_env(&l.env);
-            //code.push_lambda(&l.name.clone().unwrap_or_else(|| "[anonymous]".into()));
-
-            if !l.defines.is_empty() {
-                compile_sequence(&l.defines, code, false)?;
-            }
-            compile_sequence(&l.expressions, code, true)?;
-
-            //code.pop_lambda();
-            //code.pop_env();
-            code.push(Instruction::Return);
-            let jump_offset = code.code_size() - skip_pos - 1;
-            code.replace(skip_pos, Instruction::Jump(jump_offset));
+            code.code_blocks.push(compile_lambda(arena, l, rv)?);
+            code.push(Instruction::CreateClosure(code.code_blocks.len() - 1));
         }
         SyntaxElement::Application(a) => {
-            compile(&a.function, code, false, false)?;
+            compile(arena, &a.function, code, false, rv)?;
             code.push(Instruction::PushValue);
             for instr in a.args.iter() {
-                compile(instr, code, false, false)?;
+                compile(arena, instr, code, false, rv)?;
                 code.push(Instruction::PushValue);
             }
             code.push(Instruction::CreateFrame(a.args.len()));
@@ -150,26 +155,56 @@ pub fn compile(
             }
         }
     }
-    Ok(code.code_size() - initial_len)
+    Ok(())
 }
 
 fn compile_sequence(
+    arena: &Arena,
     expressions: &[SyntaxElement],
     code: &mut CodeBlock,
     tail: bool,
+    rv: PoolPtr,
 ) -> Result<usize, String> {
     let initial_len = code.code_size();
     for instr in expressions[..expressions.len() - 1].iter() {
-        compile(instr, code, false, false)?;
+        compile(arena, instr, code, false, rv)?;
     }
     compile(
+        arena,
         // This should have been caught at the syntax step.
         expressions.last().expect("empty sequence"),
         code,
         tail,
-        false,
+        rv,
     )?;
     Ok(code.code_size() - initial_len)
+}
+
+fn compile_lambda(arena: &Arena, l: &Lambda, rv: PoolPtr) -> Result<PoolPtr, String> {
+    let mut code = CodeBlock::new(l.name.clone(), l.arity, l.dotted, l.env.clone());
+    // See `compile_toplevel` for an explanation of rooted_vec
+    let rooted_vec = arena.insert_rooted(Value::Vector(RefCell::new(vec![])));
+
+    code.push(Instruction::CheckArity {
+        arity: l.arity,
+        dotted: l.dotted,
+    });
+    if l.dotted {
+        code.push(Instruction::PackFrame(l.arity));
+    }
+    code.push(Instruction::ExtendFrame(l.defines.len()));
+    code.push(Instruction::ExtendEnv);
+
+    if !l.defines.is_empty() {
+        compile_sequence(arena, &l.defines, &mut code, false, rooted_vec.pp())?;
+    }
+    compile_sequence(arena, &l.expressions, &mut code, true, rooted_vec.pp())?;
+
+    code.push(Instruction::Return);
+
+    let code_block_ptr = arena.insert(Value::CodeBlock(Box::new(code))).0;
+    arena.try_get_vector(ValRef(rv)).unwrap().borrow_mut().push(ValRef(code_block_ptr));
+    Ok(code_block_ptr)
 }
 
 fn make_get_instruction(altitude: usize, depth: usize, index: usize) -> Instruction {

src/vm.rs
@@ -145,15 +145,22 @@ impl Code {
 }
 
 #[derive(Debug, Clone, PartialEq)]
+pub struct ReturnPoint {
+    pub code_block: PoolPtr,
+    pub pc: usize,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Vm {
     value: ValRef,
     pc: usize,
-    return_stack: Vec<usize>,
+    return_stack: Vec<ReturnPoint>,
     stack: Vec<ValRef>,
     global_env: ValRef,
     env: ValRef,
     fun: ValRef,
     root_code_block: PoolPtr,
+    current_code_block: PoolPtr,
 }
 
 impl Vm {
@@ -163,6 +170,13 @@ impl Vm {
             debug_assert!(v.0.ok());
         }
         self.value = v;
+    }
+
+    fn get_return_point(&self) -> ReturnPoint {
+        ReturnPoint {
+            code_block: self.current_code_block,
+            pc: self.pc
+        }
     }
 }
 
@@ -175,7 +189,11 @@ impl Inventory for Vm {
         for s in self.stack.iter() {
             v.push(s.0);
         }
-        v.push(self.root_code_block)
+        v.push(self.root_code_block);
+        v.push(self.current_code_block);
+        for rp in self.return_stack.iter() {
+            v.push(rp.code_block);
+        }
     }
 }
 
@@ -196,6 +214,7 @@ pub fn run(
         env,
         fun: arena.unspecific,
         root_code_block: code.pp(),
+        current_code_block: code.pp(),
     };
     arena.root_vm(&vm);
     let res = loop {
@@ -210,8 +229,8 @@ pub fn run(
 }
 
 fn run_one_instruction(arena: &Arena, vm: &mut Vm) -> Result<bool, Error> {
-    // println!("running {:?}", code.instructions[vm.pc]);
-    let code = arena.get_code_block(ValRef(vm.root_code_block));
+    let code = arena.get_code_block(ValRef(vm.current_code_block));
+    // println!("running {:?}, rst {:?}", code.instructions[vm.pc], vm.return_stack);
     match code.instructions[vm.pc] {
         Instruction::Constant(v) => vm.set_value(ValRef(code.constants[v])),
         Instruction::JumpFalse(offset) => {
@@ -299,15 +318,19 @@ fn run_one_instruction(arena: &Arena, vm: &mut Vm) -> Result<bool, Error> {
             vm.env = vm.value;
         }
         Instruction::Return => {
-            vm.pc = vm
+            let ReturnPoint { code_block, pc } =  vm
                 .return_stack
                 .pop()
                 .expect("Returning with no values on return stack.");
+            vm.current_code_block = code_block;
+            vm.pc = pc;
         }
-        Instruction::CreateClosure(offset) => vm.set_value(arena.insert(Value::Lambda {
-            code: vm.pc + offset,
-            environment: vm.env,
-        })),
+        Instruction::CreateClosure(idx) => {
+            vm.set_value(arena.insert(Value::Lambda {
+                code: code.code_blocks[idx],
+                frame: vm.env.0,
+            }));
+        }
         Instruction::PackFrame(arity) => {
             let frame = get_activation_frame(arena, vm.value);
             let values = frame.borrow_mut().values.clone();
@@ -350,7 +373,7 @@ fn run_one_instruction(arena: &Arena, vm: &mut Vm) -> Result<bool, Error> {
                 _ => {
                     return Err(raise_string(
                         arena,
-                        format!("cannot apply non-function: {}", pretty_print(arena, fun_r)),
+                        format!("cannot pop non-function: {}", pretty_print(arena, fun_r)),
                     ));
                 }
             }
@@ -366,6 +389,8 @@ fn run_one_instruction(arena: &Arena, vm: &mut Vm) -> Result<bool, Error> {
             // We could just pop values from the stack as we add them to the frame, but
             // this causes them to become unrooted, which is bad. So we copy the values,
             // then truncate the stack.
+            // Matt from 3 months later - I don't see why the above is true. There's no
+            // allocations in this block so it's fine to temporarily unroot these values.
             let stack_len = vm.stack.len();
             for i in (0..size).rev() {
                 frame.values[i] = *vm
@@ -392,7 +417,7 @@ fn invoke(arena: &Arena, vm: &mut Vm, tail: bool) -> Result<(), Error> {
     let fun = arena.get(vm.fun);
     match fun {
         Value::Lambda {
-            code, environment, ..
+            code, frame
         } => {
             if !tail {
                 if vm.return_stack.len() > MAX_RECURSION_DEPTH {
@@ -400,10 +425,11 @@ fn invoke(arena: &Arena, vm: &mut Vm, tail: bool) -> Result<(), Error> {
                         RefCell::new("Maximum recursion depth exceeded".into()),
                     ))));
                 }
-                vm.return_stack.push(vm.pc);
+                vm.return_stack.push(vm.get_return_point());
             }
-            vm.env = *environment;
-            vm.pc = *code;
+            vm.env = ValRef(*frame);
+            vm.current_code_block = *code;
+            vm.pc = 0;
         }
         Value::Primitive(p) => match p.implementation {
             PrimitiveImplementation::Simple(i) => {
@@ -430,10 +456,12 @@ fn invoke(arena: &Arena, vm: &mut Vm, tail: bool) -> Result<(), Error> {
             }
             vm.stack = c.stack.clone();
             vm.return_stack = c.return_stack.clone();
-            vm.pc = vm
+            let ReturnPoint { code_block, pc } = vm
                 .return_stack
                 .pop()
                 .expect("Popping continuation with no return address");
+            vm.current_code_block = code_block;
+            vm.pc = pc;
             vm.set_value(af.values[0]);
         }
         _ => {
@@ -465,7 +493,7 @@ fn apply(arena: &Arena, vm: &mut Vm, tail: bool) -> Result<(), Error> {
 }
 
 fn call_cc(arena: &Arena, vm: &mut Vm) -> Result<(), Error> {
-    vm.return_stack.push(vm.pc);
+    vm.return_stack.push(vm.get_return_point());
     let cont = Continuation {
         stack: vm.stack.clone(),
         return_stack: vm.return_stack.clone(),
@@ -525,6 +553,7 @@ fn raise(arena: &Arena, vm: &Vm, abort: bool) -> Error {
     }
 }
 
+/*
 fn error_stack(arena: &Arena, vm: &Vm, code: &Code, error: Error) -> Error {
     let mut message = String::new();
     let positions = std::iter::once(&vm.pc).chain(vm.return_stack.iter().rev());
@@ -538,6 +567,7 @@ fn error_stack(arena: &Arena, vm: &Vm, code: &Code, error: Error) -> Error {
     let msg_r = arena.insert_rooted(Value::String(RefCell::new(message)));
     error.map_error(|e| arena.insert_rooted(Value::Pair(Cell::new(e.vr()), Cell::new(msg_r.vr()))))
 }
+*/
 
 fn handle_error(arena: &Arena, vm: &mut Vm, e: Error) -> Result<RootPtr, RootPtr> {
     // let annotated_e = error_stack(arena, &vm, code, e);
@@ -569,7 +599,7 @@ fn handle_error(arena: &Arena, vm: &mut Vm, e: Error) -> Result<RootPtr, RootPtr
 #[derive(Debug, Clone, PartialEq)]
 pub struct Continuation {
     stack: Vec<ValRef>,
-    return_stack: Vec<usize>,
+    return_stack: Vec<ReturnPoint>,
 }
 
 impl heap::Inventory for Continuation {

src/ast.rs
@@ -705,7 +705,7 @@ fn parse_compile_run_macro(
     val: ValRef,
 ) -> Result<ValRef, String> {
     let syntax_tree =
-        parse(arena, vms, env, af_info, val).map_err(|e| format!("Syntax error: {}", e))?;
+        parse(arena, vms, env, af_info, val).map_err(|e| format!("syntax error: {}", e))?;
     arena
         .get_activation_frame(vms.global_frame.vr())
         .borrow_mut()
@@ -713,15 +713,13 @@ fn parse_compile_run_macro(
 
     let frame = make_frame(arena, vms.global_frame.vr(), af_info);
 
-    let start_pc = vms.code.code_size();
-    compile::compile(&syntax_tree, &mut vms.code, false, true)
-        .map_err(|e| format!("Compilation error: {}", e))?;
-    vms.code.push(Instruction::Finish);
+    let code = compile::compile_toplevel(arena, &syntax_tree, vms.global_environment.clone())
+        .map_err(|e| format!("compilation error: {}", e))?;
     // println!(" => {:?}", &state.code[start_pc..state.code.len()]);
-    let code = arena.insert_rooted(Value::CodeBlock(Box::new(vms.code.clone())));
-    vm::run(arena, code, start_pc, vms.global_frame.vr(), frame)
+    let code = arena.root(ValRef(code));
+    vm::run(arena, code, 0, vms.global_frame.vr(), frame)
         .map(|v| v.vr())
-        .map_err(|e| format!("Runtime error: {}", pretty_print(arena, e.vr())))
+        .map_err(|e| format!("runtime error: {}", pretty_print(arena, e.vr())))
 }
 
 fn make_frame(arena: &Arena, global_frame: ValRef, af_info: &RcAfi) -> ValRef {

src/heap.rs
@@ -237,6 +237,11 @@ impl Pool {
         let init = selr.allocated;
         for (i_mark, mark) in selr.marked.clone().iter().enumerate() {
             if !mark && !selr.data[i_mark].is_free() {
+                /*
+                if let PoolEntry::Used(UsedPoolEntry(Value::CodeBlock(_))) = &selr.data[i_mark] {
+                    println!("Freeing code block at {:?} / {}", selr as *const Pool, i_mark);
+                }
+                */
                 selr.free_ref(u16::try_from(i_mark).unwrap(), debug)
             }
         }

src/lib.rs
@@ -25,7 +25,7 @@ use std::cell::RefCell;
 use std::fs;
 use std::rc::Rc;
 
-use arena::Arena;
+use arena::{Arena, ValRef};
 use ast::SyntaxElement;
 use environment::{ActivationFrame, ActivationFrameInfo, Environment, RcEnv};
 use heap::RootPtr;
@@ -53,7 +53,6 @@ pub const ERROR_HANDLER_INDEX: usize = 0;
 pub struct VmState {
     pub global_environment: RcEnv,
     pub global_frame: RootPtr,
-    pub code: CodeBlock,
 }
 
 impl VmState {
@@ -80,7 +79,6 @@ impl VmState {
         VmState {
             global_environment: global_environment.clone(),
             global_frame,
-            code: CodeBlock::new(Some("[toplevel]".into()), 0, false, &global_environment),
         }
     }
 }
@@ -113,7 +111,7 @@ pub fn parse_compile_run(
             .len(),
     }));
     let syntax_tree = ast::parse(arena, state, &cloned_env, &global_af_info, read.vr())
-        .map_err(|e| format!("Syntax error: {}", e))?;
+        .map_err(|e| format!("syntax error: {}", e))?;
     arena
         .get_activation_frame(state.global_frame.vr())
         .borrow_mut()
@@ -127,18 +125,17 @@ pub fn compile_run(
     state: &mut VmState,
     syntax_tree: &SyntaxElement,
 ) -> Result<RootPtr, String> {
-    let start_pc = state.code.code_size();
-    compile::compile(&syntax_tree, &mut state.code, false, true)
-        .map_err(|e| format!("Compilation error: {}", e))?;
-    state.code.push(Instruction::Finish);
-    // println!(" => {:?}", &state.code[start_pc..state.code.len()]);
-    let code = arena.insert_rooted(Value::CodeBlock(Box::new(state.code.clone())));
+    let code = compile::compile_toplevel(arena, &syntax_tree, state.global_environment.clone())
+        .map_err(|e| format!("compilation error: {}", e))?;
+    let code = arena.root(ValRef(code));
+    // println!(" => {:?}", arena.get_code_block(code.vr()));
+    // println!(" => {:?}", arena.get_code_block(ValRef(arena.get_code_block(code.vr()).code_blocks[0])));
     vm::run(
         arena,
         code,
-        start_pc,
+        0,
         state.global_frame.vr(),
         state.global_frame.vr(),
     )
-    .map_err(|e| format!("Runtime error: {}", pretty_print(arena, e.vr())))
+    .map_err(|e| format!("runtime error: {}", pretty_print(arena, e.vr())))
 }

src/value.rs
@@ -25,6 +25,7 @@ use primitives::{Port, Primitive, SyntacticClosure};
 use vm::Continuation;
 use {heap, util};
 use compile::CodeBlock;
+use heap::PoolPtr;
 
 // TODO box some of these, values are currently 56 bytes long oh no
 // TODO remove PartialEq and Clone. Clone should only be used in the numeric primitives library.
@@ -49,7 +50,7 @@ pub enum Value {
     Pair(Cell<ValRef>, Cell<ValRef>),
     ByteVector(RefCell<Vec<u8>>),
     Vector(RefCell<Vec<ValRef>>),
-    Lambda { code: usize, environment: ValRef },
+    Lambda { code: PoolPtr, frame: PoolPtr },
     Port(Box<Port>),
     Primitive(&'static Primitive),
     ActivationFrame(RefCell<ActivationFrame>),
@@ -115,8 +116,9 @@ impl heap::Inventory for Value {
                     v.push(val.0);
                 }
             }
-            Value::Lambda { environment, .. } => {
-                v.push(environment.0);
+            Value::Lambda { code, frame } => {
+                v.push(*code);
+                v.push(*frame);
             }
             Value::ActivationFrame(af) => {
                 let f = af.borrow();

commit 4040cc143cff03537d17480e5f169ad5943a25ef

Fix arity checking
This had been broken since cc897c89acd18c6811179c8cf6c717a8aeaa0260
due to careless handling of the program counter when loading a procedure.

The PC was set to 0 (the first instruction in the code block), but
subsequently incremented in the VM loop, thus skipping the arity check
(always the first instruction).

src/compile.rs
@@ -192,6 +192,7 @@ fn compile_lambda(arena: &Arena, l: &Lambda, rv: PoolPtr) -> PoolPtr {
         .unwrap()
         .borrow_mut()
         .push(code_block_ptr);
+    // println!("{:?}", code_block_ptr.pretty_print());
     code_block_ptr
 }
 

src/primitives/mod.rs
@@ -256,6 +256,7 @@ pub struct Primitive {
     pub implementation: PrimitiveImplementation,
 }
 
+#[derive(Copy, Clone)]
 pub enum PrimitiveImplementation {
     Simple(fn(&Arena, &[PoolPtr]) -> Result<PoolPtr, String>),
     Io(fn(&Arena, PoolPtr, PoolPtr, &[PoolPtr]) -> Result<PoolPtr, String>),

src/vm.rs
@@ -126,6 +126,8 @@ pub enum Instruction {
 #[derive(Debug, Clone, PartialEq)]
 pub struct ReturnPoint {
     pub code_block: PoolPtr,
+
+    /// Holds the index of the last executed instruction
     pub pc: usize,
 }
 
@@ -210,8 +212,9 @@ pub fn run(code: RootPtr, pc: usize, env: PoolPtr, int: &Interpreter) -> Result<
 fn run_one_instruction(int: &Interpreter, vm: &mut Vm) -> Result<bool, Error> {
     let arena = &int.arena;
     let code = vm.current_code_block.long_lived().get_code_block();
-    // println!("running {:?}, rst {:?}", code.instructions[vm.pc], vm.return_stack);
-    match code.instructions[vm.pc] {
+    let instr = code.instructions[vm.pc];
+    // println!("running {:?}, pc {}", instr, vm.pc);
+    match instr {
         Instruction::Constant(v) => vm.set_value(code.constants[v]),
         Instruction::JumpFalse(offset) => {
             if !vm.value.truthy() {
@@ -287,7 +290,7 @@ fn run_one_instruction(int: &Interpreter, vm: &mut Vm) -> Result<bool, Error> {
         }
         Instruction::CheckArity { arity, dotted } => {
             let actual_arity = vm
-                .env
+                .value
                 .long_lived()
                 .get_activation_frame()
                 .borrow()
@@ -334,6 +337,7 @@ fn run_one_instruction(int: &Interpreter, vm: &mut Vm) -> Result<bool, Error> {
             let frame = vm.value.long_lived().get_activation_frame();
             let values = frame.borrow_mut().values.clone();
             if values.len() < arity {
+                // TODO should this be a panic? Normally we've already checked arg length here
                 return Err(raise_string(
                     arena,
                     format!(
@@ -410,7 +414,10 @@ fn run_one_instruction(int: &Interpreter, vm: &mut Vm) -> Result<bool, Error> {
         Instruction::NoOp => panic!("NoOp encountered."),
         Instruction::Finish => return Ok(true),
     }
-    vm.pc += 1;
+    match instr {
+        Instruction::FunctionInvoke { .. } => {}
+        _ => vm.pc += 1,
+    }
     Ok(false)
 }
 
@@ -430,32 +437,38 @@ fn invoke(int: &Interpreter, vm: &mut Vm, tail: bool) -> Result<(), Error> {
             vm.current_code_block = *code;
             vm.pc = 0;
         }
-        Value::Primitive(p) => match p.implementation {
-            PrimitiveImplementation::Simple(i) => {
-                let af = vm.value.long_lived().get_activation_frame();
-                let values = &af.borrow().values;
-                vm.set_value(
-                    i(arena, &values)
-                        .map_err(|e| raise_string(arena, format!("In {:?}: {}", p, e)))?,
-                );
-            }
-            PrimitiveImplementation::Io(i) => {
-                let af = vm.value.long_lived().get_activation_frame();
-                let values = &af.borrow().values;
-                let global_env = vm.global_env.long_lived().get_activation_frame().borrow();
-                let input_port = global_env.values[INPUT_PORT_INDEX];
-                let output_port = global_env.values[OUTPUT_PORT_INDEX];
-                vm.set_value(
-                    i(arena, input_port, output_port, &values)
-                        .map_err(|e| raise_string(arena, format!("In {:?}: {}", p, e)))?,
-                );
-            }
-            PrimitiveImplementation::Apply => apply(int, vm, tail)?,
-            PrimitiveImplementation::CallCC => call_cc(int, vm)?,
-            PrimitiveImplementation::Abort => return Err(raise(arena, vm, true)),
-            PrimitiveImplementation::Raise => return Err(raise(arena, vm, false)),
-            PrimitiveImplementation::Eval => eval(int, vm)?,
-        },
+        Value::Primitive(p) => {
+            match p.implementation {
+                PrimitiveImplementation::Simple(i) => {
+                    let af = vm.value.long_lived().get_activation_frame();
+                    let values = &af.borrow().values;
+                    vm.set_value(
+                        i(arena, &values)
+                            .map_err(|e| raise_string(arena, format!("In {:?}: {}", p, e)))?,
+                    );
+                }
+                PrimitiveImplementation::Io(i) => {
+                    let af = vm.value.long_lived().get_activation_frame();
+                    let values = &af.borrow().values;
+                    let global_env = vm.global_env.long_lived().get_activation_frame().borrow();
+                    let input_port = global_env.values[INPUT_PORT_INDEX];
+                    let output_port = global_env.values[OUTPUT_PORT_INDEX];
+                    vm.set_value(
+                        i(arena, input_port, output_port, &values)
+                            .map_err(|e| raise_string(arena, format!("In {:?}: {}", p, e)))?,
+                    );
+                }
+                PrimitiveImplementation::Apply => apply(int, vm, tail)?,
+                PrimitiveImplementation::CallCC => call_cc(int, vm)?,
+                PrimitiveImplementation::Abort => return Err(raise(arena, vm, true)),
+                PrimitiveImplementation::Raise => return Err(raise(arena, vm, false)),
+                PrimitiveImplementation::Eval => eval(int, vm)?,
+            };
+            match p.implementation {
+                PrimitiveImplementation::Apply | PrimitiveImplementation::CallCC => {}
+                _ => vm.pc += 1,
+            };
+        }
         Value::Continuation(c) => {
             let af = vm.value.long_lived().get_activation_frame().borrow();
             if af.values.len() != 1 {
@@ -471,7 +484,7 @@ fn invoke(int: &Interpreter, vm: &mut Vm, tail: bool) -> Result<(), Error> {
                 .pop()
                 .expect("popping continuation with no return address");
             vm.current_code_block = code_block;
-            vm.pc = pc;
+            vm.pc = pc + 1;
             vm.set_value(af.values[0]);
         }
         _ => {
@@ -608,7 +621,6 @@ fn handle_error(int: &Interpreter, vm: &mut Vm, e: Error) -> Result<RootPtr, Roo
                     vm.fun = handler;
                     vm.set_value(arena.insert(Value::ActivationFrame(RefCell::new(frame))));
                     invoke(int, vm, false).map_err(|e| e.into_value())?;
-                    vm.pc += 1;
                     Ok(arena.root(arena.unspecific))
                 }
                 _ => {

tests/integration_tests.rs
@@ -434,3 +434,10 @@ fn eval() {
         .unwrap()
     );
 }
+
+#[test]
+fn check_arity() {
+    assert!(magic_execute("((lambda (x) x))", false).is_err());
+    assert!(magic_execute("(call/cc)", true).is_err());
+    assert!(magic_execute("((syntax-rules -1))", true).is_err());
+}